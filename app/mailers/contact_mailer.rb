0cbe7beb9abc93a1c124e5f54e34a68239650a15
sunkencity999 <theroyalart@gmail.com>
2014-09-08 21:17:16 -0700

    changed default "to" email to test production functionality

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: "hansingebretsen@yahoo.com"
+  default to: "christopher@scratchifoff.co"
 
   def contact_email(name, email, body)
     @name = name
